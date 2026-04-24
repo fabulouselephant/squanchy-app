89e09fbf5af90be37dc3fa9da67fd61ec96b2f27
Anna Vertegel <annavertegel@Annas-MacBook-Pro.local>
2026-04-24 08:03:21 +0300

    Set next app, fetch data, display data

---
+++ app/src/components/CharachterCard/CharachterCard.styled.ts
@@ -0,0 +1,7 @@
+import { Typography, Stack } from "@mui/material"
+import { styled } from '@mui/material/styles'
+
+export const CharachterDescripionLine = styled(Stack)({
+    fontFamily: 'PTSans', 
+    fontSize: '16px'
+})
