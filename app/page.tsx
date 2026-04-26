0fae1c010896ff036cf5da889c6c47385d779a29
Anna Vertegel <annavertegel@Annas-MacBook-Pro.local>
2026-04-26 18:01:03 +0300

    Fix issue with hydration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 'use client'
-import { CharacterCard } from './components/CharacterCard/CharacterCard'
 
+import dynamic from 'next/dynamic'
+
+const CharacterCard = dynamic(() => import('./components/CharacterCard/CharacterCard').then((m) => m.CharacterCard),
+{ ssr: false } 
+)
 
 export default function Home() {
   return <CharacterCard />
